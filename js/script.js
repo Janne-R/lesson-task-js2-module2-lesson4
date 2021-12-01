be39f34225de679406eb6f42c4468f093f0b03b7
Janne-R <74651035+Janne-R@users.noreply.github.com>
2021-12-01 20:02:36 +0100

    api failing

---
+++ js/script.js
@@ -0,0 +1,27 @@
+import Developer from "./Developer.js";
+
+(async function () {
+  // select before the try so we can use it in the catch too
+  const developerContainer = document.querySelector(".developer-container");
+
+  try {
+    const url = "https://noroffcors.herokuapp.com/https://api.rawg.io/api/developers";
+    const response = await fetch(url);
+    const json = await response.json();
+
+    console.log(json);
+
+    const developers = json.results;
+
+    developerContainer.innerHTML = "";
+
+    // create HTML for each item using a class
+    developers.forEach(function (developer) {
+      const dev = new Developer(developer.name, developer.image_background);
+      developerContainer.innerHTML += dev.render();
+    });
+  } catch (error) {
+    console.log(error);
+    developerContainer.innerHTML = error;
+  }
+})();
